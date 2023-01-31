e1f69fa97c31a41946c1263147bb137280b81947
Reza Banankhah <banankha@usc.edu>
2023-01-31 14:25:49 -0800

    Delete .svelte-kit directory
    Shouldn't be tracked

--- .svelte-kit/generated/nodes/0.js
+++
@@ -1,4 +0,0 @@
-import * as universal from "../../../src/routes/+layout.ts";
-export { universal };
-export { default as component } from "../../../src/routes/+layout.svelte";
-export const has_server_load = true;

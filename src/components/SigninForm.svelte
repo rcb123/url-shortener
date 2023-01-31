f20fd3139ea7a58e101b4ad36d3a295bfef223c2
Reza Banankhah <banankha@usc.edu>
2023-01-31 14:58:59 -0800

    Changed sign up form to serve as sign in form

--- src/components/SigninForm.svelte
+++ src/components/SigninForm.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <div class="lg:container mx-auto h-full w-full p-8">
-	<form method="POST" action="/signup" use:enhance class="flex flex-col items-center w-full justify-center">
-		<h1 class="text-3xl font-medium text-center my-2">Register</h1>
+	<form method="POST" action="/signin" use:enhance class="flex flex-col items-center w-full justify-center">
+		<h1 class="text-3xl font-medium text-center my-2">Login</h1>
 		<div class="form-control w-full max-w-xs">
 			<label for="email" class="label">
 				<span class="label-text">Email</span>
@@ -38,40 +38,8 @@
 				{/if}
 			</label>
 		</div>
-		<div class="form-control w-full max-w-xs">
-			<label for="passwordConfirm" class="label">
-				<span class="label-text">Confirm Password</span>
-			</label>
-			<input
-				type="password"
-				name="passwordConfirm"
-				class="input w-full max-w-xs {form?.errors?.passwordConfirm
-					? 'input-error'
-					: 'input-bordered'}"
-			/>
-			<label for="passwordConfirm" class="label">
-				{#if form?.errors?.passwordConfirm}
-					<span class="label-text-alt text-error">{form?.errors?.passwordConfirm}</span>
-				{/if}
-			</label>
-		</div>
-		<div class="form-control w-full max-w-xs pb-2">
-			<label class="label cursor-pointer justify-start space-x-4">
-				<input
-					type="checkbox"
-					name="terms"
-					class="checkbox checkbox-primary {form?.errors?.terms ? 'border-error' : ''}"
-				/>
-				<span class="label-text">I accept the terms and conditions</span>
-			</label>
-			<label for="terms" class="label">
-				{#if form?.errors?.terms}
-					<span class="label-text-alt text-error">{form?.errors?.terms}</span>
-				{/if}
-			</label>
-		</div>
 		<div class="w-full max-w-xs">
-			<button class="btn btn-primary w-full" type="submit">Register</button>
+			<button class="btn btn-primary w-full" type="submit">Login</button>
 		</div>
 	</form>
 </div>

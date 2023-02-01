71f21c016d8a9e689697db6741d9b99e20fdbfec
Reza Banankhah <banankha@usc.edu>
2023-02-01 01:54:24 -0800

    Styling changes
    Made it smaller to fit into pages better

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -14,43 +14,29 @@
 </div>
 
 <style lang="scss">
-	*,
-	*::before,
-	*::after {
-		padding: 0;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
 
-	body {
-		background-color: #000;
-		min-height: 100vh;
-		display: grid;
-		place-items: center;
-	}
-
-	$duration: 4s;
+	$duration: 1.5s;
 
 	.loader {
 		position: relative;
 		width: 21em;
 		height: 21em;
-		background-color: #000;
-		filter: blur(0.2em) contrast(2);
+		
+		filter: blur(0.03em) contrast(2);
 		animation: loaderSpin $duration * 4 infinite linear;
 
 		@keyframes loaderSpin {
 			to {
-				transform: rotate(360deg);
+				transform: rotate(360);
 			}
 		}
 
 		& > i {
 			position: absolute;
 			left: 10em;
-			top: 10em;
-			width: 1em;
-			height: 1em;
+			top: 3em;
+			width: 0.4em;
+			height: 0.4em;
 			background-color: hsl(var(--hue, 0), 75%, 75%);
 			border-radius: 50%;
 			transform: rotateZ(var(--rz, 0)) translateY(5em);
@@ -58,17 +44,17 @@
 
 			@keyframes iMove {
 				0% {
-					transform: rotateZ(var(--rz, 0)) translateY(4em) translate(0, 0) scale(0);
+					transform: rotateZ(var(--rz, 0)) translateY(1.4em) translate(0, 0) scale(0);
 				}
 				2% {
-					transform: rotateZ(var(--rz, 0)) translateY(4em) translate(0, 0) scale(1.25);
+					transform: rotateZ(var(--rz, 0)) translateY(1.4em) translate(0, 0) scale(1.25);
 				}
 				20% {
-					transform: rotateZ(var(--rz, 0)) translateY(4em) translate(0, 0) scale(1.25);
+					transform: rotateZ(var(--rz, 0)) translateY(1.4em) translate(0, 0) scale(1.25);
 				}
 				90%,
 				100% {
-					transform: rotateZ(var(--rz, 0)) translateY(4em) translate(var(--tx, 0), var(--ty, 0))
+					transform: rotateZ(var(--rz, 0)) translateY(1.4em) translate(var(--tx, 0), var(--ty, 0))
 						scale(0);
 				}
 			}
@@ -76,9 +62,9 @@
 			@for $i from 0 to 72 {
 				&:nth-child(#{$i + 1}) {
 					--rz: #{$i * 5}deg;
-					--delay: #{$duration / 72 * $i - $duration};
-					--tx: #{random(1000) / 250}em;
-					--ty: #{random(1000) / 125 - 2.5}em;
+					--delay: #{calc($duration / 72 * $i - $duration)};
+					--tx: #{calc(random(900) / 250)}em;
+					--ty: #{calc(random(900) / 125 - 2.5)}em;
 					--hue: #{$i * 5};
 				}
 			}

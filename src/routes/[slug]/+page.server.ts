d783f8f65ca31f1facd2b51d70d0eee8d37c597c
Reza Banankhah <banankha@usc.edu>
2023-03-09 13:22:12 -0800

    Simplified slug redirecting
    Throws redirect serverside during page load instead of performing operations after page load

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,42 +1,25 @@
 import type { PageServerLoad } from './$types';
+import { redirect, error } from '@sveltejs/kit';
 import { supabase } from '$lib/supabaseClient';
 
 export const load: PageServerLoad = (async ({ params }) => {
 	const slug = params.slug;
 
-	try {
-		const response = await supabase.from('shortLink').select().eq('slug', slug);
-		if (response.data) {
-			if (response.data.length != 0) {
-				const clicksinc = response.data[0].clicks + 1;
-				const { error } = await supabase
-					.from('shortLink')
-					.update({ clicks: `${clicksinc}` })
-					.eq('slug', `${slug}`);
-				if (error) {
-					console.log(error);
-					return {
-						url: '/',
-						status: 500
-					}
-				}
-
-				return {
-					url: `${response.data[0].url}`,
-					status: 302
-				};
-			} else {
-				return {
-					url: '/',
-					status: 404
-				};
+	const response = await supabase.from('shortLink').select().eq('slug', slug);
+	if (response.data) {
+		if (response.data.length != 0) {
+			const clicksinc = response.data[0].clicks + 1;
+			const increment = await supabase
+				.from('shortLink')
+				.update({ clicks: `${clicksinc}` })
+				.eq('slug', `${slug}`);
+			if (increment.error) {
+				throw error(500, increment.error)
 			}
+
+			throw redirect(302, `${response.data[0].url}`);
+		} else {
+			throw error(404, 'Short link not found!');
 		}
-	} catch (error) {
-		console.error(error);
-		return {
-			error: error,
-			status: 500
-		};
 	}
 }) satisfies PageServerLoad;

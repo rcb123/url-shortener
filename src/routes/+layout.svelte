a76b13aec092c705d5f69076a086810cd41bf2ba
Reza Banankhah <banankha@usc.edu>
2023-08-05 08:19:26 -0700

    Small update
    Changed title to reflect app name
    Added source code and portfolio link to all pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,3 +21,10 @@
 <div>
 	<slot />
 </div>
+
+<div>
+	<a class="absolute right-10 bottom-12 text-lg" href="https://github.com/rcb123/url-shortener"
+		>&lt;/&gt; GitHub</a
+	>
+	<a class="absolute right-10 bottom-5 text-lg" href="https://rezab.vercel.app/">🖥️ Portfolio</a>
+</div>
